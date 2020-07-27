3c578676efdc1f51475f2201a755da784590914c
OhSva <somkamolv@hotmail.com>
2020-07-28 00:42:07 +0700

    try adding components

---
+++ src/components/MyInfo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function MyApp() {
+    return (
+        <div>
+            <h1>Oh Sva</h1>
+            <p>This is paragraph about me..</p>
+            <ul>
+                <li>Thailand</li>
+                <li>Japan</li>
+                <li>England</li>
+            </ul>
+        </div>
+    )
+}
+
+export default MyApp;
